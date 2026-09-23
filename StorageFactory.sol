427be404c547b701eb9fb5f537fd5ee84074c6e9
Yuvi8990 <yuvrajsinghji89@gmail.com>
2026-09-23 10:45:38 +0530

    feat: implement factory pattern to deploy and track SimpleStorage contracts

---
+++ StorageFactory.sol
@@ -0,0 +1,21 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.34;
+
+import{SimpleStorage} from "./SimpleStorage.sol"; 
+contract StorageFactory{
+
+  SimpleStorage[] public listOfSimpleStorageContracts;
+
+  function createSimpleStorageContract() public{
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+  }
+
+  function sfStore(uint256 _simpleStorageIndex, uint256 _newFavSimpleStorageNumber) public{
+      listOfSimpleStorageContracts[_simpleStorageIndex].store(_newFavSimpleStorageNumber);
+         }
+
+  function sfGet(uint _simpleStorageIndex) public view returns(uint256){
+      return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();  
+  }
+}
